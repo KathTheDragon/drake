a2a9b8c5c871bfd34b0f5cbac4ca6515dd5c1e6d
KathTheDragon <kaththedragon@gmail.com>
2019-10-03 22:43:33 +0100

    Add a few new nodes

--- drake/ast.py
+++ drake/ast.py
@@ -1,18 +1,33 @@
 import enum
 from dataclasses import dataclass
+from typing import List
 from .lexer import Token
 
 class ASTNode():
     def pprint(self):
         raise NotImplementedError
 
-
+# Deprecate Primary in favour of Literal and Identifier
 @dataclass
 class Primary(ASTNode):
     value: Token
 
     def pprint(self):
         return f'{self.value.type} {self.value.value}'
+
+@dataclass
+class Literal(ASTNode):
+    value: Token
+
+@dataclass
+class Identifier(ASTNode):
+    name: Token
+    local: bool = True
+
+@dataclass
+class UnaryOp(ASTNode):
+    operand: ASTNode
+    operator: Token
 
 @dataclass
 class BinaryOp(ASTNode):
@@ -31,6 +46,18 @@ class BinaryOp(ASTNode):
             br = '\n'
         return f'Binary {self.operator.value} ({br}{left},{br}{right}{br})'
 
+@dataclass
+class Assignment(ASTNode):
+    name: Token
+    expression: ASTNode
+    local: bool = True
+
+@dataclass
+class Block(ASTNode):
+    expressions: List[ASTNode]
+
+    def __iter__(self):
+        yield from self.expressions
 
 class Precedence(enum.IntEnum):
     NONE       = enum.auto()
