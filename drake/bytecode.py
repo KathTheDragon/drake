39c1d864391de9320a310aae13bf3d1942d79793
KathTheDragon <kaththedragon@gmail.com>
2019-10-04 10:57:21 +0100

    Add assembler and disassembler

--- drake/bytecode.py
+++ drake/bytecode.py
@@ -62,3 +62,40 @@ OPS = NULLARY_OPS + UNARY_OPS + BINARY_OPS + TERNARY_OPS
 
 ## Classes
 Op = enum.Enum('Op', ' '.join(OPS), start=0)
+
+## Functions
+def assemble(instructions):
+    bytecode = bytesarray()
+    for instruction in instructions:
+        op = instruction[0]
+        opname = op._name_
+        if opname in NULLARY_OPS:
+            oplength = 1
+        elif opname in UNARY_OPS:
+            oplength = 2
+        elif opname in BINARY_OPS:
+            oplength = 3
+        elif opname in TERNARY_OPS:
+            oplength = 4
+        if oplength == len(instruction):
+            bytecode.append(op._value_)
+            bytecode.extend(instruction[1:])
+    return bytecode
+
+def disassemble(bytecode):
+    ip = 0
+    while ip < len(bytecode):
+        op = Op(bytecode[ip])
+        opname = op._name_
+        if opname in NULLARY_OPS:
+            oplength = 1
+        elif opname in UNARY_OPS:
+            oplength = 2
+        elif opname in BINARY_OPS:
+            oplength = 3
+        elif opname in TERNARY_OPS:
+            oplength = 4
+        nextip = ip + oplength
+        args = bytecode[ip+1:nextip]
+        ip = nextip
+        yield tuple(op, *args)
