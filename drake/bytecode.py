128a02f83f2bec2a09ad9e1fb0f3ed29379d7e56
KathTheDragon <kaththedragon@gmail.com>
2019-10-04 12:57:58 +0100

    Wrap bytecode.assemble and .disassemble into a Bytecode class

--- drake/bytecode.py
+++ drake/bytecode.py
@@ -60,35 +60,40 @@ OPS = NULLARY_OPS + UNARY_OPS + BINARY_OPS
 ## Classes
 Op = enum.Enum('Op', ' '.join(OPS), start=0)
 
-## Functions
-def assemble(instructions):
-    bytecode = bytesarray()
-    for instruction in instructions:
-        op = instruction[0]
-        opname = op._name_
-        if opname in NULLARY_OPS:
-            oplength = 1
-        elif opname in UNARY_OPS:
-            oplength = 2
-        elif opname in BINARY_OPS:
-            oplength = 3
-        if oplength == len(instruction):
-            bytecode.append(op._value_)
-            bytecode.extend(instruction[1:])
-    return bytecode
+class Bytecode(bytearray):
+    def __repr__(self):
+        contents = ''.join((fr'\x{hex(byte)[2:]:0>2}' for byte in self))
+        return f"Bytecode(b'{contents}')"
 
-def disassemble(bytecode):
-    ip = 0
-    while ip < len(bytecode):
-        op = Op(bytecode[ip])
-        opname = op._name_
-        if opname in NULLARY_OPS:
-            oplength = 1
-        elif opname in UNARY_OPS:
-            oplength = 2
-        elif opname in BINARY_OPS:
-            oplength = 3
-        nextip = ip + oplength
-        args = bytecode[ip+1:nextip]
-        ip = nextip
-        yield tuple(op, *args)
+    @staticmethod
+    def assemble(instructions):
+        bytecode = Bytecode()
+        for instruction in instructions:
+            op = instruction[0]
+            opname = op._name_
+            if opname in NULLARY_OPS:
+                oplength = 1
+            elif opname in UNARY_OPS:
+                oplength = 2
+            elif opname in BINARY_OPS:
+                oplength = 3
+            if oplength == len(instruction):
+                bytecode.append(op._value_)
+                bytecode.extend(instruction[1:])
+        return bytecode
+
+    def disassemble(self):
+        ip = 0
+        while ip < len(self):
+            op = Op(self[ip])
+            opname = op._name_
+            if opname in NULLARY_OPS:
+                oplength = 1
+            elif opname in UNARY_OPS:
+                oplength = 2
+            elif opname in BINARY_OPS:
+                oplength = 3
+            nextip = ip + oplength
+            args = self[ip+1:nextip]
+            ip = nextip
+            yield (op,) + tuple(args)
