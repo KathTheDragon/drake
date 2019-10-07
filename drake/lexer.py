1d2913f2f7cd2a15d6091f7c637e247f6b42996b
KathTheDragon <kaththedragon@gmail.com>
2019-10-07 13:50:15 +0100

    Strip quotes from string literals

--- drake/lexer.py
+++ drake/lexer.py
@@ -84,6 +84,8 @@ def lex(source):
             column = match.start()
             if type in ('COMMENT', 'WHITESPACE'):
                 continue
+            elif type == 'STRING':
+                value = value[1:-1]
             elif type in ('IMAG_INTEGER', 'IMAG_DECIMAL'):
                 value = value.strip('j')
             yield Token(type, value, linenum, column)
