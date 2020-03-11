97f8c38728dd004d7da0f4a2c2e7b28441f2f1df
KathTheDragon <kaththedragon@gmail.com>
2020-03-11 03:47:58 +0000

    Add subscription as a way of making parametrised types

--- drake/types.py
+++ drake/types.py
@@ -6,6 +6,12 @@ class Type:
     name: str
     params: tuple = ()
 
+    def __getitem__(self, item):
+        if isinstance(item, tuple):
+            return Type(self.name, item)
+        else:
+            return Type(self.name, (item,))
+
 ## Exceptions
 @dataclass
 class TypeMismatch(Exception):
