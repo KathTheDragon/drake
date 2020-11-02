420f86b39cc0a50645313535050299f652de20f6
KathTheDragon <kaththedragon@gmail.com>
2020-11-02 05:22:56 +0000

    Prep test_parser and test_analyser for tests

--- tests/test_analyser.py
+++ tests/test_analyser.py
@@ -1,7 +1,3 @@
 import pytest
-import re
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from drake import analyser
-from drake.parser import Parser
-from drake.ast import *
+from drake.src import analyser
+from drake.src.ast import *
---
+++ tests/test_parser.py
@@ -0,0 +1,2 @@
+import pytest
+from drake.src import parser
